20ec4708b3ec41852f288d6cd2e1937b0bbcefe3
Rushan Shaymardanov <rush.ru@gmail.com>
2015-01-25 17:26:31 +0500

    Added kitchen fsm

--- main.c
+++ main.c
@@ -13,6 +13,8 @@
 #define MSG_BUTTON0_LONG 1
 #define MSG_BUTTON1_SHORT 2
 #define MSG_BUTTON1_LONG 3
+#define MSG_BUTTON2_SHORT 4
+#define MSG_BUTTON2_LONG 5
 
 #define BUTTON_NOISE_DELAY 25
 #define BUTTON_LONG_PUSH 500
@@ -28,9 +30,19 @@
 #define STATE_ROOM_CHAN0_WAIT_SWITCH 2
 #define STATE_ROOM_LIGHT 3
 
+#define STATE_KITCHEN_DARK 0
+#define STATE_KITCHEN_LIGHTS_ONE 1
+#define STATE_KITCHEN_LIGHTS 2
+
+#define BUTTON0_DDR DDRC
+#define BUTTON0_PIN PINC
+#define BUTTON0_BIT PC4
 #define BUTTON1_DDR DDRC
 #define BUTTON1_PIN PINC
 #define BUTTON1_BIT PC5
+#define BUTTON2_DDR DDRD
+#define BUTTON2_PIN PIND
+#define BUTTON2_BIT PD0
 
 #define CHAN_DDR DDRC
 #define CHAN_PORT PORTC
@@ -41,8 +53,8 @@
 #define CHAN3_BIT PC3
 
 
-uint8_t button1_state = STATE_BUTTON_RELEASED;
-uint8_t room_state = STATE_ROOM_DARK;
+uint8_t button0_state = STATE_BUTTON_RELEASED, button1_state = STATE_BUTTON_RELEASED, button2_state = STATE_BUTTON_RELEASED;
+uint8_t room_state = STATE_ROOM_DARK, kitchen_state = STATE_KITCHEN_DARK;
 
 
 ISR(TIMER0_OVF_vect) {
@@ -132,7 +144,39 @@ void room_fsm() {
             break;
     }
 }
-    
+
+void kitchen_fsm() {
+    switch ( kitchen_state ) {
+        case STATE_KITCHEN_DARK:
+            if ( msg_get(MSG_BUTTON0_SHORT) ) {
+                CHAN_PORT |= 1<<CHAN2_BIT|1<<CHAN3_BIT;
+                kitchen_state = STATE_KITCHEN_LIGHTS;
+            } else if ( msg_get(MSG_BUTTON2_SHORT) ) {
+                CHAN_PORT |= 1<<CHAN3_BIT;
+                kitchen_state = STATE_KITCHEN_LIGHTS_ONE;
+            }
+            break;
+        case STATE_KITCHEN_LIGHTS_ONE:
+            if ( msg_get(MSG_BUTTON0_SHORT) ) {
+                CHAN_PORT |= 1<<CHAN2_BIT;
+                kitchen_state = STATE_KITCHEN_LIGHTS;
+            } else if (msg_get(MSG_BUTTON2_SHORT) ) {
+                CHAN_PORT &= ~(1<<CHAN3_BIT);
+                kitchen_state= STATE_KITCHEN_DARK;
+            }
+            break;
+        case STATE_KITCHEN_LIGHTS:
+            if ( msg_get(MSG_BUTTON0_SHORT) ) {
+                CHAN_PORT &= ~(1<<CHAN2_BIT|1<<CHAN3_BIT);
+            }
+            break;
+        default:
+            CHAN_PORT &= ~(1<<CHAN2_BIT|1<<CHAN3_BIT);
+            kitchen_state = STATE_KITCHEN_DARK;
+            break;
+    }
+}
+
 
 int main() {
     TCCR0 |= 1<<CS01;
@@ -142,7 +186,9 @@ int main() {
     CHAN_DDR |= 1<<CHAN0_BIT | 1<<CHAN1_BIT | 1<<CHAN2_BIT | 1<<CHAN3_BIT;
     sei();
     while(1) { 
+        button0_state = button_fsm(button0_state, TIMER_BUTTON0, MSG_BUTTON0_SHORT, MSG_BUTTON0_LONG, &BUTTON0_PIN, BUTTON0_BIT);
         button1_state = button_fsm(button1_state, TIMER_BUTTON1, MSG_BUTTON1_SHORT, MSG_BUTTON1_LONG, &BUTTON1_PIN, BUTTON1_BIT);
+        button2_state = button_fsm(button2_state, TIMER_BUTTON2, MSG_BUTTON2_SHORT, MSG_BUTTON2_LONG, &BUTTON2_PIN, BUTTON2_BIT);
         room_fsm();
         msg_process();
     }
